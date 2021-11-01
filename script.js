7c2558db0ea46fc599ae878504708ff502547e83
Marcus Assunção <guieduardo80@gmail.com>
2021-11-01 03:09:59 -0300

    Acescentando algumas mudanças na pagina

--- script.js
+++ script.js
@@ -15,3 +15,24 @@ function escrever () {
 setInterval(() => {
     escrever()
 }, 2000);
+
+function Modais (idmod,Mod) {
+    const mod = document.getElementById(idmod)
+    const div = document.getElementById(Mod)
+    mod.style.display = 'flex'
+    mod.addEventListener('click',(e) => {
+        if(e.target.id === idmod || e.target.id === Mod) {
+            mod.style.display = 'none'
+        }
+    })
+}
+
+const Link = document.querySelector('.link')
+Link.addEventListener('click', () => {
+    Modais('fundo1','date')
+})
+
+const Git = document.querySelector('.github')
+Git.addEventListener('click', () => {
+    Modais('fundo2','Git')
+})
